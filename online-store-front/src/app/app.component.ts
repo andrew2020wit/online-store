adaf66dcc653fc795a9aa11fc9ed888e2e492cc3
andrew2020wit <andrew2020wit@gmail.com>
2020-11-15 22:02:41 +0200

    remove styles

--- online-store-front/src/app/app.component.ts
+++ online-store-front/src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@app/auth-module/auth.service';
-import { Observable } from 'rxjs';
 import { menuList } from './site-menu';
-import { IStylesState, StylesStateService } from './styles-state.service';
 
 @Component({
   selector: 'app-root',
@@ -13,21 +11,11 @@ import { IStylesState, StylesStateService } from './styles-state.service';
 export class AppComponent implements OnInit {
   menuOn = true;
   links = menuList;
-  styleState$: Observable<IStylesState>;
 
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-    private stylesStateService: StylesStateService
-  ) {
+  constructor(private authService: AuthService, private router: Router) {
     this.authService.loadLocalToken();
-    this.styleState$ = this.stylesStateService.stylesState$;
   }
   ngOnInit(): void {}
-
-  setTheme(str: string) {
-    this.stylesStateService.setTheme(str);
-  }
 
   toHome() {
     this.router.navigate(['']);
--- online-store-front/src/app/auth-module/view/login-form/login-form.component.ts
+++ online-store-front/src/app/auth-module/view/login-form/login-form.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '@app/auth-module/auth.service';
-import { IStylesState, StylesStateService } from '@app/styles-state.service';
-import { Observable } from 'rxjs';
 import { LoginDto } from '../../dto/login.dto';
 
 @Component({
@@ -12,18 +10,15 @@ import { LoginDto } from '../../dto/login.dto';
 })
 export class LoginFormComponent implements OnInit {
   newLoginForm: FormGroup;
-  styleState$: Observable<IStylesState>;
 
   constructor(
     private formBuilder: FormBuilder,
-    private authService: AuthService,
-    private stylesStateService: StylesStateService
+    private authService: AuthService
   ) {
     this.newLoginForm = this.formBuilder.group({
       login: ['', [Validators.required]],
       password: ['', [Validators.required]],
     });
-    this.styleState$ = this.stylesStateService.stylesState$;
   }
 
   ngOnInit(): void {}
