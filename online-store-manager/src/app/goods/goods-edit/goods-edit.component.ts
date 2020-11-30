d2df478e8abed3c374282d8601abbf29f34f4371
andrew2020wit <andrew2020wit@gmail.com>
2020-11-30 14:02:23 +0200

    GoodsEditComponent remove GQL

--- online-store-manager/src/app/goods/goods-edit/goods-edit.component.ts
+++ online-store-manager/src/app/goods/goods-edit/goods-edit.component.ts
@@ -1,16 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { Apollo, QueryRef } from 'apollo-angular';
 import { baseApiUrl } from '../../../environments/environment';
 import { formFieldsGoodsEdit } from '../formFields.const';
-import { IGoods } from '../goods.interface';
-import {
-  CreateGoodsGQL,
-  DisActiveGoodsGQL,
-  EditGoodsGQL,
-  GoodsOneGQL,
-} from '../GoodsOneGQL.const';
+import { GoodsEntity } from './../goods.entity';
+import { GoodsService } from './../goods.service';
 
 class FormModel {
   name = '';
@@ -30,7 +24,6 @@ export class GoodsEditComponent implements OnInit {
   formModel = new FormModel();
   formFields = formFieldsGoodsEdit;
 
-  GoodsWatchQuery: QueryRef<any>;
   goodsId: string;
 
   createdOn: Date;
@@ -42,7 +35,10 @@ export class GoodsEditComponent implements OnInit {
   bigApiEndpoint = '';
   smallApiEndpoint = '';
 
-  constructor(private apollo: Apollo, private activateRoute: ActivatedRoute) {
+  constructor(
+    private service: GoodsService,
+    private activateRoute: ActivatedRoute
+  ) {
     this.goodsId = this.activateRoute.snapshot.params['id'];
     this.bigApiEndpoint =
       baseApiUrl + '/goods/photo-upload/' + 'bigPhotoUrl/' + this.goodsId;
@@ -59,84 +55,65 @@ export class GoodsEditComponent implements OnInit {
       console.log('no goodsId');
       return;
     }
-    this.apollo
-      .watchQuery<any>({
-        query: GoodsOneGQL,
-        variables: {
-          id: this.goodsId,
-        },
-      })
-      .valueChanges.subscribe(({ data, loading }) => {
-        const formModel1 = new FormModel();
-        const goods1 = data.getOneGoods as IGoods;
-        console.log('goods1', goods1);
 
-        formModel1.name = goods1.name;
-        formModel1.description = goods1.description;
-        formModel1.price = goods1.price;
-        formModel1.smallPhotoUrl = goods1.smallPhotoUrl;
-        formModel1.bigPhotoUrl = goods1.bigPhotoUrl;
+    this.service.getById(this.goodsId).subscribe((goods1) => {
+      const formModel1 = new FormModel();
 
-        this.formModel = formModel1;
+      console.log('goods1', goods1);
 
-        this.createdOn = goods1.createdOn;
-        this.updatedOn = goods1.updatedOn;
-        this.bigPhotoUrl = baseApiUrl + this.formModel.bigPhotoUrl;
-        this.smallPhotoUrl = baseApiUrl + this.formModel.smallPhotoUrl;
-      });
+      formModel1.name = goods1.name;
+      formModel1.description = goods1.description;
+      formModel1.price = goods1.price;
+      formModel1.smallPhotoUrl = goods1.smallPhotoUrl;
+      formModel1.bigPhotoUrl = goods1.bigPhotoUrl;
+
+      this.formModel = formModel1;
+
+      this.createdOn = goods1.createdOn;
+      this.updatedOn = goods1.updatedOn;
+      this.bigPhotoUrl = baseApiUrl + this.formModel.bigPhotoUrl;
+      this.smallPhotoUrl = baseApiUrl + this.formModel.smallPhotoUrl;
+    });
   }
 
   edit() {
     if (!this.goodsId) {
       return;
     }
-    this.apollo
-      .mutate({
-        mutation: EditGoodsGQL,
-        variables: {
-          id: this.goodsId,
-          name: this.formModel.name,
-          description: this.formModel.description,
-          smallPhotoUrl: this.formModel.smallPhotoUrl,
-          bigPhotoUrl: this.formModel.bigPhotoUrl,
-          price: this.formModel.price,
-        },
-      })
-      .subscribe((x) => {
-        location.reload();
-      });
+
+    const newGoods: GoodsEntity = {
+      id: this.goodsId,
+      name: this.formModel.name,
+      description: this.formModel.description,
+      smallPhotoUrl: this.formModel.smallPhotoUrl,
+      bigPhotoUrl: this.formModel.bigPhotoUrl,
+      price: this.formModel.price,
+    };
+    this.service.edit$(newGoods).subscribe((x) => {
+      console.log('service.edit', x);
+    });
   }
 
   create() {
     if (this.goodsId) {
       return;
     }
-    this.apollo
-      .mutate({
-        mutation: CreateGoodsGQL,
-        variables: {
-          name: this.formModel.name,
-          description: this.formModel.description,
-          smallPhotoUrl: this.formModel.smallPhotoUrl,
-          bigPhotoUrl: this.formModel.bigPhotoUrl,
-          price: this.formModel.price,
-        },
-      })
-      .subscribe((x) => {
-        location.reload();
-      });
+    const newGoods: GoodsEntity = {
+      name: this.formModel.name,
+      description: this.formModel.description,
+      smallPhotoUrl: this.formModel.smallPhotoUrl,
+      bigPhotoUrl: this.formModel.bigPhotoUrl,
+      price: this.formModel.price,
+    };
+    this.service.create$(newGoods).subscribe((x) => {
+      console.log('service.create', x);
+    });
   }
 
   disActivate() {
-    if (!this.goodsId) {
-      return;
-    }
-    this.apollo
-      .mutate({
-        mutation: DisActiveGoodsGQL,
-        variables: { goodsId: this.goodsId },
-      })
-      .subscribe((x) => console.log(x));
+    this.service.activate$(this.goodsId, false).subscribe((x) => {
+      console.log('service.create', x);
+    });
   }
 
   doneBigImgUpload(isOk) {
