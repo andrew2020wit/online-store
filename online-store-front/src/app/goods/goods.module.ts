64fe42bc3a3643f7c7adc3cf0e8d85f5ea913742
andrew2020wit <andrew2020wit@gmail.com>
2020-12-17 17:24:19 +0200

    PriceSpanComponent

--- online-store-front/src/app/goods/goods.module.ts
+++ online-store-front/src/app/goods/goods.module.ts
@@ -8,12 +8,19 @@ import { GoodsCardComponent } from './goods-card/goods-card.component';
 import { GoodsListComponent } from './goods-list/goods-list.component';
 import { GoodsPageComponent } from './goods-page/goods-page.component';
 import { GoodsViewComponent } from './goods-view/goods-view.component';
+import { PriceSpanComponent } from './price-span/price-span.component';
 
-const exportModules = [GoodsListComponent, GoodsCardComponent];
+const exportModules = [
+  GoodsListComponent,
+  GoodsPageComponent,
+  GoodsViewComponent,
+  PriceSpanComponent,
+  GoodsCardComponent,
+];
 
 @NgModule({
-  declarations: [...exportModules, GoodsPageComponent, GoodsViewComponent],
-  exports: [...exportModules],
+  declarations: [...exportModules],
+  exports: exportModules,
   imports: [
     CommonModule,
     MaterialShareModule,
---
+++ online-store-front/src/app/goods/price-span/price-span.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-price-span',
+  templateUrl: './price-span.component.html',
+  styleUrls: ['./price-span.component.scss']
+})
+export class PriceSpanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
